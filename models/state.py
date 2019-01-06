7e3f4a046f52a578f18321cd863a663aa1745dab
Heindrick Cheung <hcheung01@gmail.com>
2019-01-06 22:18:56 +0000

    update

--- models/state.py
+++ models/state.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 """This is the state class"""
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+
+    __tablename__ = "states"
+
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref=backref("state", cascade="all, delete-orphan"))
